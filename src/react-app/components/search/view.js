fc6bdd9f62b85d65f4cca09497bfd5d6ec91f4a4
btzr-io <btzr.io@gmail.com>
2020-10-22 03:16:35 -0500

    add create-new button

--- src/react-app/components/search/view.js
+++ src/react-app/components/search/view.js
@@ -1,17 +1,39 @@
 import React from "react";
 import Icon from "@mdi/react";
 import { TASK_STATUS, TASK_STATUS_TYPES } from "@app/constants/types";
+import {mdiPlusCircleOutline, mdiPlusCircle } from "@mdi/js";
 
 function StatusButton({ status }) {
   const { icon, color } = TASK_STATUS_TYPES[status];
   return (
-    <button className="status_button">
+    <button className="button button--status">
       <Icon
         path={icon}
         data-color={color}
         size={1}
         className={"status_icon"}
         spin={status === TASK_STATUS.RUNING && 0.64}
+      />
+    </button>
+  );
+}
+
+function AddItemButton() {
+  const [hover, setHover] = React.useState(false);
+  const handleHover = () => {
+    setHover(true)
+  }
+
+  const handleNotHover = () => {
+    setHover(false)
+  }
+
+  return (
+    <button className="button button--add" onMouseEnter={handleHover} onFocus={handleHover} onBlur={handleNotHover} onMouseLeave={handleNotHover}>
+      <Icon
+        path={!hover ? mdiPlusCircleOutline : mdiPlusCircle}
+        size={1.2}
+        className={"icon--button"}
       />
     </button>
   );
@@ -80,7 +102,10 @@ function SearchInput({ onChange }) {
 function Search({ tasks, searchTasksByTitle, searchResults }) {
   return (
     <div>
-      <SearchInput onChange={searchTasksByTitle} />
+      <div className="panel_header">
+        <SearchInput onChange={searchTasksByTitle} />
+        <AddItemButton />
+      </div>
       <SearchResults entries={tasks} results={searchResults.tasks} />
     </div>
   );
