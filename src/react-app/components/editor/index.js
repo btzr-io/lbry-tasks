31c5ce08f19a1d19a22ceb1bdcd801dd83546dfd
btzr-io <btzr.io@gmail.com>
2020-10-24 22:05:13 -0500

    implement react-flow

--- src/react-app/components/editor/index.js
+++ src/react-app/components/editor/index.js
@@ -1,10 +1,31 @@
 import React from "react";
+import ReactFlow, { Background } from "react-flow-renderer";
+const elements = [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Node 1" },
+    position: { x: 50, y: 5 },
+  },
+  // you can also pass a React Node as a label
+  { id: "2", data: { label: <div>Node 2</div> }, position: { x: 50, y: 100 } },
+  { id: "e1-2", source: "1", target: "2", animated: true },
+];
 
 function Editor() {
   return (
     <div className="panel editor">
       <div className="editor_toolbar" />
-      <div className="editor_viwer" />
+      <div className="editor_viwer">
+        <ReactFlow elements={elements}>
+          <Background
+            variant="dots"
+            gap={32}
+            size={1}
+            style={{ opacity: 0.8 }}
+          />
+        </ReactFlow>
+      </div>
     </div>
   );
 }
